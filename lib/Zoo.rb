2f1ca286ec9b28e7602d928e762ed24023e8f754
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-11-23 10:02:47 -0800

    tests pass

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,35 @@
 class Zoo
+    attr_reader :name, :location
+
+    @@all = []
+    def initialize(name, location)
+        @name = name 
+        @location = location
+        @@all << self 
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    def animals 
+        Animal.all.select {|animal| animal.zoo == self.name}
+    end 
+
+    def animal_species 
+        animals.map {|animal| animal.species}.uniq
+    end 
+
+    def find_by_species(species)
+        animals.select {|zoo| zoo.species == species}
+    end 
+
+    def animal_nicknames
+        animals.map {|zoo| zoo.nickname}
+    end 
+
+    def self.find_by_location(location)
+        Zoo.all.select {|zoo| zoo.location == location}
+    end 
 
 end
--- run.rb
+++ run.rb
@@ -3,7 +3,13 @@ require_relative "lib/Zoo.rb"
 require 'pry'
 
 
-#Test your code here
+dog = Animal.new("Dog", 100, "Mitch", "Lincoln Park Zoo")
+zebra = Animal.new("Zebra", 200, "Meghan", "Point Defiance Zoo") 
+lion = Animal.new("Lion", 300, "Dylan", "Point Defiance Zoo")
+
+tacoma = Zoo.new("Point Defiance Zoo", "Tacoma")
+chi = Zoo.new("Lincoln Park Zoo", "Chicago")
+
 
 
 binding.pry
